f40173338bd672587f295d52cd3ba0cb000606f7
benjaminfmoore <benjaminfmoore@yahoo.com>
2020-06-11 16:50:04 -0400

    heroku deployment setup

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 const express = require('express');
+const path = require('path');
 
 // import db
 require('../db/mongoose');
@@ -16,6 +17,15 @@ app.use(express.json());
 // Call routes
 app.use(userRouter);
 app.use(apiRouter);
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, '../client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', (request, response) => {
+    response.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Express server is up on port ${port}`);
