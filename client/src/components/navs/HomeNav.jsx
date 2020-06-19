78536646ca8d32fad7ffea332cec280f6d5f72dc
ghostkid130 <urena.reynold+git@gmail.com>
2020-06-19 01:29:14 -0400

    Added drop down panel template in home page

--- client/src/components/navs/HomeNav.jsx
+++ client/src/components/navs/HomeNav.jsx
@@ -1,20 +1,33 @@
 import React from 'react'
 
-import CartButton from '../buttons/CartButton'
-import downIcon from '../../assets/imgs/homeDownIcon.svg'
 import './nav.css'
+import CartButton from '../buttons/CartButton'
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
 
 const HomeNav = ({history}) => {
     return (
         <div className="home-nav nav-shadow"> 
             <div>
                 <h1 id="home-OTG-title">On-the-Go</h1>
-                <div id="home-store-container">
+                <ExpansionPanel>
+                    <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
                     <p id="home-store">Store_Name</p>
-                    <p id="home-store-zip">(33031)</p>
-                    <img src={downIcon} />
-                </div>
-            </div>
+                    </ExpansionPanelSummary>
+                        <ExpansionPanelDetails>
+                        <div className="home-store-container">
+                            <p>store 1</p>
+                            <p>store 2</p>
+                            <p>store 3</p>
+                            <p>store 4</p>
+                            <p>store 5</p>
+                        </div>
+                        </ExpansionPanelDetails>
+                </ExpansionPanel>
+            </div> 
             <div>
                 <CartButton history={history}/>
             </div>
