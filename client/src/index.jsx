4881fa46ae6fea4c1f1ce1159f6bf49a437e8402
Rey <46139207+ghostkid130@users.noreply.github.com>
2020-06-20 11:20:16 -0400

    Update index.jsx
    Duplicate Providers. Moving:
    - CartProvider && SearchProvider --> App.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,19 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { CartContextProvider } from './context/CartContext';
-import { SearchContextProvider } from './context/SearchContext';
-
-
 import { UserContextProvider } from './context/UserContext'
 
 ReactDOM.render( 
     <UserContextProvider>
-    <CartContextProvider>
-    <SearchContextProvider>
         <App />
-    </SearchContextProvider>
-    </CartContextProvider>
     </UserContextProvider> 
     , document.getElementById('root')
 );
