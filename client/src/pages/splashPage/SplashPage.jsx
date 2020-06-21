71a490bf46759182960c72b0747b1999f043e405
Benjamin Moore <62070683+benjaminfmoore@users.noreply.github.com>
2020-06-21 14:24:56 -0400

    created a function to allow to log in as a guest (#59)

--- client/src/pages/splashPage/SplashPage.jsx
+++ client/src/pages/splashPage/SplashPage.jsx
@@ -1,70 +1,85 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
 
-import LoginDrawer from './LoginDrawer'
-import SignUpDrawer from './SignUpDrawer'
+import LoginDrawer from './LoginDrawer';
+import SignUpDrawer from './SignUpDrawer';
 
-import { Button } from '@material-ui/core'
-import logo from '../../assets/imgs/cartLogo/Logo.svg'
-import './splashPage.css'
-import '../../components/buttons/button.css'
+import { Button } from '@material-ui/core';
+import logo from '../../assets/imgs/cartLogo/Logo.svg';
+import './splashPage.css';
+import '../../components/buttons/button.css';
 
 const SplashPage = () => {
-    const [ signUpDrawer, setSignUpDrawer] = useState(false)
-    const [ loginDrawer, setLoginDrawer] = useState(false)
+  const [signUpDrawer, setSignUpDrawer] = useState(false);
+  const [loginDrawer, setLoginDrawer] = useState(false);
 
-    
-    const toggleDrawer = (open) => (event) => {
-        setSignUpDrawer(open); setLoginDrawer(open)
-    }
+  const toggleDrawer = (open) => (event) => {
+    setSignUpDrawer(open);
+    setLoginDrawer(open);
+  };
 
+  function handleGuestLogin(e) {
+    e.preventDefault();
+    axios({
+      method: 'POST',
+      url: `/users/login`,
+      data: {
+        email: 'Guest@guestaccount.com',
+        password: '123apple'
+      }
+    })
+      .then(({ data }) => {
+        //setUser(data.user)
+        localStorage.setItem('token', data.token);
+        setLoggedIn(true);
+        setLoginEmail('');
+        setPass('');
+        setUser({ firstName: data.user.firstName, email: data.user.email });
+        toggleDrawer(false);
+        history.push('/home');
+      })
+      .catch((e) => console.log(e.message.toString(), 'Crendentials error'));
+  }
 
-    return (
-        <div className="splash-container">
-            <div className="splash-top">
-                <h2>On-the-Go</h2>
-                <h3>Shopping</h3>
-                <p>Skip the checkout lines</p>
-            </div>
+  return (
+    <div className="splash-container">
+      <div className="splash-top">
+        <h2>On-the-Go</h2>
+        <h3>Shopping</h3>
+        <p>Skip the checkout lines</p>
+      </div>
 
-            <img src={logo} alt="On-the-shop shopping cart logo"/>
+      <img src={logo} alt="On-the-shop shopping cart logo" />
 
-            <div className="splash-bottom">
-                <Button
-                    variant="contained"
-                    className="button-lg-green"
-                    onClick={() => setLoginDrawer(true)}>
-                        Login
-                </Button>
-                <Button 
-                    variant="contained" 
-                    className="button-lg-hollow" 
-                    onClick={() => setSignUpDrawer(true)}
-                >
-                    Signup
-                </Button>
+      <div className="splash-bottom">
+        <Button
+          variant="contained"
+          className="button-lg-green"
+          onClick={() => setLoginDrawer(true)}
+        >
+          Login
+        </Button>
+        <Button
+          variant="contained"
+          className="button-lg-hollow"
+          onClick={() => setSignUpDrawer(true)}
+        >
+          Signup
+        </Button>
 
-                <Button 
-                    className="elusive-button" 
-                >
-                    Continue as guest
-                </Button>
+        <Button className="elusive-button" onClick={handleGuestLogin()}>
+          Continue as guest
+        </Button>
 
-                <LoginDrawer 
-                    toggleDrawer={toggleDrawer}
-                    loginDrawer={loginDrawer} 
-                    
-                />
+        <LoginDrawer toggleDrawer={toggleDrawer} loginDrawer={loginDrawer} />
 
-                <SignUpDrawer 
-                    toggleDrawer={toggleDrawer}
-                    signUpDrawer={signUpDrawer} 
-                    setSignUpDrawer={setSignUpDrawer}
+        <SignUpDrawer
+          toggleDrawer={toggleDrawer}
+          signUpDrawer={signUpDrawer}
+          setSignUpDrawer={setSignUpDrawer}
+        />
+      </div>
+    </div>
+  );
+};
 
-                />
-
-            </div>
-        </div>
-    )
-}
-
-export default SplashPage
+export default SplashPage;
